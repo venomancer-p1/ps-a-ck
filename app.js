14e097ee8add7b012a4912be75c0a39289717183
venomancer-p1 <venomancer777@protonmail.com>
2022-06-17 15:59:09 -0400

    commit

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const extendTimeoutMiddleware = (req, res, next) => {
     let isDataSent = false;
 
     // Only extend the timeout for API requests
-    if (!req.url.includes('/get') || !req.url.includes('/token')) {
+    if (!req.url.includes('/get') && !req.url.includes('/token')) {
         next();
         return;
     }
